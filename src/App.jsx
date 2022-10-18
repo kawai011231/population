fb75c70343dd4ad64e9b4ff3555a578d66987919
kawai011231 <yousuke011231@gmail.com>
2022-10-18 11:38:38 +0900

    headerを追加

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className={styles.header}>都道府県人口推移グラフ</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
